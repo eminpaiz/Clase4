96a69c124bdc1e353fc94a41ffd8dc575d873580
Emin Paiz <emin_pc_@hotmail.com>
2019-04-27 18:31:18 -0600

    clase4

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ejemplo_Clase4.web.Startup))]
+namespace Ejemplo_Clase4.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
